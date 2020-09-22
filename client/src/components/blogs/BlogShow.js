a60370c6652cbd4e50e483d63d571d91fcb2a653
Vassilis <vassilispapadop@gmail.com>
2020-09-22 15:34:48 +0200

    final version

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,13 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage(){
+    if(this.props.blog.imageUrl){
+      //we need to append the aws url for the specific bucket
+      return <img src={this.props.blog.imageUrl} />;
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +25,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
